41289d4c5281d69073df38823bcbc8384cdd95f6
Playhack029 <130815176+Playhack029@users.noreply.github.com>
2023-04-16 11:13:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 alert("Halo Selamat Datang Di Web Saya, Web Masih Dalam Tahap Pengembangan, Thank You Sudah Mampir");
+
+function clock() {
+      var jam = document.getElementById("hour")
+      var menit = document.getElementById("minute")
+      var detik = document.getElementById("second")
+      
+      var hours = new Date().getHours()
+      var minutes = new Date().getMinutes()
+      var seconds = new Date().getSeconds()
+      
+      jam.innerHTML = hours
+      menit.innerHTML = minutes
+      detik.innerHTML = seconds
+    }
+    
+    var inter = setInterval(clock, 1000)
+    
+    var date = new Date();
+    
+    document.getElementById("dt").innerHTML = date.toDateString();
